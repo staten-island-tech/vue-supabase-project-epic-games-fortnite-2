a77ebe7dfa428990d87b7711c0eb865a44ccaca4
yi <73318855+Shatterdest@users.noreply.github.com>
2024-04-04 13:02:51 -0400

    f

--- paint/src/App.vue
+++ paint/src/App.vue
@@ -1,21 +1,10 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient.js'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
 </script>
 
 <template>
-<div>
-  
-</div>
+  <div>
+    <h1>hello world!</h1>
+  </div>
 </template>
