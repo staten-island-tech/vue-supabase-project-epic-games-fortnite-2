824dcdd75e901d50a7594251d08572584bcbdd12
yi <73318855+Shatterdest@users.noreply.github.com>
2024-04-04 12:45:21 -0400

    Initial Commit

---
+++ paint/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient.js'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
+<template>
+<div>
+  
+</div>
+</template>
---
+++ paint/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://tpmidxhprynkamvxzvvd.supabase.co', 'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbWlkeGhwcnlua2Ftdnh6dnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyNDg4NzksImV4cCI6MjAyNzgyNDg3OX0')
