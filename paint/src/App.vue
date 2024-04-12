132b4b436360b35dcc88714da910645f7dac6d95
aalskdjfhg123 <jiangryan8@gmail.com>
2024-04-11 22:56:15 -0400

    look at the box go wait whered it go

--- paint/src/App.vue
+++ paint/src/App.vue
@@ -1,10 +1,83 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient.js'
+interface boardDisplay {
+  rows: number;
+  columns: number;
+  tileSize: number;
+}
+/* interface playerPos {
+  x: number;
+  y: number;
+} */
+
+const buttons = [
+  {
+    direction: "left",
+    keyCode: 37,
+  },
+  {
+    direction: "top",
+    keyCode: 38,
+  },
+  {
+    direction: "right",
+    keyCode: 39,
+  },
+  {
+    direction: "bottom",
+    keyCode: 40,
+  }
+];
+
+
+const canvas = ref();
+const ctx = ref();
+
+const boardConfig: boardDisplay = {
+  rows: 20,
+  columns: 20,
+  tileSize: 25
+}
+
+let playerLocation/* : playerPos */ = {
+  x: ref(10),
+  y: ref(10),
+  
+}
+
+
+onMounted(()=>{
+  canvas.value = document.getElementById("canvas");
+  ctx.value = canvas.value.getContext("2d");
+  canvas.value.height = boardConfig.rows * boardConfig.tileSize
+  canvas.value.width = boardConfig.columns * boardConfig.tileSize
+  ctx.value.fillStyle="black";
+  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
+  
+})  
+
+ function move(){
+  ctx.value.fillStyle="white";
+  ctx.value.fillRect(0,0, boardConfig.rows*boardConfig.tileSize, boardConfig.columns*boardConfig.tileSize )
+  playerLocation.x.value --
+  ctx.value.fillStyle="black";
+  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
+} 
+
 </script>
 
-<template>
-  <div>
-    <h1>hello world!</h1>
-  </div>
+<template @keyup="move">
+<canvas id="canvas"></canvas>
+<button @click="move"> heyfhiw</button>
 </template>
+
+<style lang="scss" scoped>
+
+
+  #canvas{
+    border: 1px solid black;
+  }
+
+
+</style>
---
+++ paint/src/vues/grid.vue
@@ -0,0 +1,26 @@
+<!-- <template>
+    <div class="stuff">
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.stuff{
+    display: grid;
+}
+</style> -->
