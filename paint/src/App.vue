24390b1a89e35253c2442613706dfc12869ee0a2
katherine <116115224+kaerulean@users.noreply.github.com>
2024-04-17 12:38:02 -0400

    jjjjj

--- paint/src/App.vue
+++ paint/src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient.js'
+const number = ref(1)
 </script>
 
 <template>
   <div>
     <h1>hello world!</h1>
   </div>
+  <div class="grid-container">
+  <div @click="number++">{{ number }}</div>
+  <div>2</div>
+  <div>3</div>  
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+</div>
 </template>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 10px;
+  background-color: #2196F3;
+  padding: 10px;
+}
+
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid black;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
+
+
+
+
