e365f6454e11c32f1908581675c1c8cfc7374c99
yi <73318855+Shatterdest@users.noreply.github.com>
2024-04-30 23:39:47 -0400

    askfjals;df

---
+++ paint/src/components/CreateWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Create a World</h2>
+    <button @click="$emit('close')">Close</button>
+    <form @submit.prevent="">
+        <submit>Create!</submit>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- paint/src/views/WorldsView.vue
+++ paint/src/views/WorldsView.vue
@@ -1,14 +1,45 @@
 <template>
-  <div v-if="sessionStore.expires > Math.floor(Date.now() / 1000)">worlds</div>
-  <div v-else>Please <router-link to="/login">log in</router-link> first to access your worlds!</div>
+  <div v-if="sessionStore.expires > Math.floor(Date.now() / 1000)">
+    <button @click="toggleCreateScreen()">create a world</button>
+    <CreateWorld v-show="showCreate" @close="toggleCreateScreen" />
+  </div>
+  <div v-else>
+    Please <router-link to="/login">log in</router-link> first to access your worlds!
+  </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, toRefs } from 'vue'
+import { onMounted, ref } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 import { useSessionStore } from '@/stores/user'
+import CreateWorld from '../components/CreateWorld.vue'
 
 const sessionStore = useSessionStore()
+const showCreate = ref(false)
+
+onMounted(async () => {
+  if (sessionStore.expires < Math.floor(Date.now() / 1000)) {
+    console.log(sessionStore.expires, Math.floor(Date.now() / 1000))
+    try {
+      const { error } = await supabase.auth.signOut()
+      if (error) throw error
+      sessionStore.$reset()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+})
+
+function toggleCreateScreen() {
+  switch (showCreate.value) {
+    case true:
+      showCreate.value = false
+      break
+    case false:
+      showCreate.value = true
+      break
+  }
+}
 </script>
 
 <style scoped></style>
