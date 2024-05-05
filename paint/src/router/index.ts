ed9aeaa2f86c911ce2cf8d1d15381ce4d8cbaa8c
aalskdjfhg123 <jiangryan8@gmail.com>
2024-05-05 12:05:04 -0400

    v

--- paint/src/router/index.ts
+++ paint/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import UserLogin from '@/views/UserLogin.vue'
 import UserRegister from '@/views/UserRegister.vue'
 import WorldsView from '@/views/WorldsView.vue'
+import World from '@/views/world.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -11,7 +12,8 @@ const router = createRouter({
     { path: '/', component: HomeView },
     { path: '/login', name: 'login', component: UserLogin },
     { path: '/register', name: 'register', component: UserRegister },
-    { path: '/worlds', name:'worlds', component: WorldsView }
+    { path: '/worlds', name:'worlds', component: WorldsView },
+    { path: '/world', name:'world', component: World },
   ]
 })
 
