cf7960c81d2168d66a26b239dfe106fbbfbdd63a
aalskdjfhg123 <jiangryan8@gmail.com>
2024-05-10 12:48:47 -0400

    commit 78

--- paint/src/views/world.vue
+++ paint/src/views/world.vue
@@ -6,14 +6,14 @@ interface boardDisplay {
   rows: number;
   columns: number;
   tileSize: number;
-} 
- interface playerPos {
+}
+interface playerPos {
   x: Ref<number>;
   y: Ref<number>;
 }
 
 
-let placedStuff: {x:number, y:number, block:string}[] = []
+let placedStuff: { x: number, y: number, block: string }[] = []
 
 
 interface gameData {
@@ -30,25 +30,25 @@ const boardConfig: boardDisplay = {
   tileSize: 25
 }
 
-let playerLocation : playerPos  = {
+let playerLocation: playerPos = {
   x: ref(12),
   y: ref(13),
 }
 
-let gameData : gameData = {
-  worldsize: {boardConfig},
+let gameData: gameData = {
+  worldsize: { boardConfig },
   placedBLocks: [placedStuff]
 }
 
 
-const keyPresses:{key:string, color:string}[] = [
+const keyPresses: { key: string, color: string }[] = [
   {
     key: "KeyX",
     color: "White"
   },
   {
-   key: "Digit1",
-   color: "Green"
+    key: "Digit1",
+    color: "Green"
   },
   {
     key: "Digit2",
@@ -56,112 +56,116 @@ const keyPresses:{key:string, color:string}[] = [
   }
 ]
 
-onMounted(()=>{
+onMounted(() => {
   canvas.value = document.getElementById("canvas");
   ctx.value = canvas.value.getContext("2d");
   canvas.value.height = boardConfig.rows * boardConfig.tileSize
-  canvas.value.width = boardConfig.columns * boardConfig.tileSize
-  ctx.value.fillStyle="black";
-  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
-  window.addEventListener("keydown", function(keydown){
-     mover(keydown)
-   })
-   
-  window.addEventListener("keydown", function(keydown: KeyboardEvent){
+  canvas.value.width = boardConfig.columns * boardConfig.tileSize;
+  ctx.value.fillStyle = "white";
+  ctx.value.fillRect(0, 0, boardConfig.rows * boardConfig.tileSize, boardConfig.columns * boardConfig.tileSize)
+  ctx.value.fillStyle = "black";
+  ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+  window.addEventListener("keydown", function (keydown) {
+    mover(keydown)
+  })
+
+  window.addEventListener("keydown", function (keydown: KeyboardEvent) {
     const keyPressed = keyPresses.find(c => c.key === keydown.code);
-    if(keyPressed != undefined){
+    if (keyPressed != undefined) {
       place(keyPressed.color)
     }
-  } ) 
-})  
+  })
+})
 
 
-function replace(){
-  for( let i=0; i < placedStuff.length; i++ ){
-    placedStuff.forEach((block) => rplace(block.x,block.y,block.block))
+function replace() {
+  const replacing = placedStuff.find(block => block.x === playerLocation.x.value && block.y === playerLocation.y.value);
+  if (replacing != undefined) {
+    rplace(replacing.x, replacing.y, replacing.block)
   }
-  function rplace(x:number,y:number,block:string){
-    ctx.value.fillStyle = block;
-    ctx.value.fillRect(boardConfig.rows*x,boardConfig.rows*y, boardConfig.tileSize, boardConfig.tileSize)
-     }
 }
-function moveLeft(){
-  if(playerLocation.x.value != 0){
-  ctx.value.fillStyle="white";
-  ctx.value.fillRect(0,0, boardConfig.rows*boardConfig.tileSize, boardConfig.columns*boardConfig.tileSize )
-  playerLocation.x.value --
-  replace()
-  ctx.value.fillStyle="black";
-  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
-}}
-function moveUp(){
-  if(playerLocation.y.value != 0){
-  ctx.value.fillStyle="white";
-  ctx.value.fillRect(0,0, boardConfig.rows*boardConfig.tileSize, boardConfig.columns*boardConfig.tileSize )
-  playerLocation.y.value --
-  replace()
-  ctx.value.fillStyle="black";
-  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
-} }
-function moveDown(){
-  if(playerLocation.y.value != boardConfig.rows-1){
-  ctx.value.fillStyle="white";
-  ctx.value.fillRect(0,0, boardConfig.rows*boardConfig.tileSize, boardConfig.columns*boardConfig.tileSize )
-  playerLocation.y.value ++
-  replace()
-  ctx.value.fillStyle="black";
-  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
-} }
-function moveRight(){
-  if(playerLocation.x.value !=boardConfig.columns-1 ){
-  ctx.value.fillStyle="white";
-  ctx.value.fillRect(0,0, boardConfig.rows*boardConfig.tileSize, boardConfig.columns*boardConfig.tileSize )
-  playerLocation.x.value ++
-  replace()
-  ctx.value.fillStyle="black";
-  ctx.value.fillRect(boardConfig.rows *playerLocation.x.value,boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize )
-} }
+function rplace(x: number, y: number, block: string) {
+  ctx.value.fillStyle = block;
+  ctx.value.fillRect(boardConfig.rows * x, boardConfig.rows * y, boardConfig.tileSize, boardConfig.tileSize)
+}
+
+function moveLeft() {
+  if (playerLocation.x.value != 0) {
+    ctx.value.fillStyle = "white";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+    replace()
+    playerLocation.x.value--
+    ctx.value.fillStyle = "black";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+  }
+}
+function moveUp() {
+  if (playerLocation.y.value != 0) {
+    ctx.value.fillStyle = "white";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+    replace()
+    playerLocation.y.value--
+    ctx.value.fillStyle = "black";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+  }
+}
+function moveDown() {
+  if (playerLocation.y.value != boardConfig.rows - 1) {
+    ctx.value.fillStyle = "white";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+    replace()
+    playerLocation.y.value++
+    ctx.value.fillStyle = "black";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+  }
+}
+function moveRight() {
+  if (playerLocation.x.value != boardConfig.columns - 1) {
+    ctx.value.fillStyle = "white";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+    replace()
+    playerLocation.x.value++
+    ctx.value.fillStyle = "black";
+    ctx.value.fillRect(boardConfig.rows * playerLocation.x.value, boardConfig.rows * playerLocation.y.value, boardConfig.tileSize, boardConfig.tileSize)
+  }
+}
 
 
- function place(block: string){
+function place(block: string) {
   ctx.value.fillStyle = `${block}`;
   let x = playerLocation.x.value
   let y = playerLocation.y.value
+  ctx.value.fillRect(boardConfig.rows * (x), boardConfig.rows * (y), boardConfig.tileSize, boardConfig.tileSize)
   placedStuff.push({
-    x:(x),
-    y:(y),
-    block:`${block}`
-  })  
-  ctx.value.fillRect(boardConfig.rows *(x),boardConfig.rows*(y), boardConfig.tileSize, boardConfig.tileSize)  
+    x: (x),
+    y: (y),
+    block: `${block}`
+  })
 }
 
-    function mover(key: KeyboardEvent) {
-    if(key.code === "ArrowLeft"){
-      moveLeft()
-    }else if(key.code === "ArrowRight"){
-      moveRight()
-    }else if( key.code === "ArrowUp"){
-      moveUp()
-    }else if(key.code === "ArrowDown"){
-      moveDown()
-    }
-   
-  } 
+function mover(key: KeyboardEvent) {
+  if (key.code === "ArrowLeft") {
+    moveLeft()
+  } else if (key.code === "ArrowRight") {
+    moveRight()
+  } else if (key.code === "ArrowUp") {
+    moveUp()
+  } else if (key.code === "ArrowDown") {
+    moveDown()
+  }
 
- 
+}
+
+
 </script>
 
 <template>
-<canvas id="canvas"></canvas>
+  <canvas id="canvas"></canvas>
 
 </template>
 
 <style lang="scss" scoped>
-
-
-  #canvas{
-    border: 1px solid black;
-  }
-
-
+#canvas {
+  border: 1px solid black;
+}
 </style>
