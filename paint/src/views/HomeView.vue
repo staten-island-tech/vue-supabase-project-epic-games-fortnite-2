d3fdfb6def0a3fbe691718cce23f30f256695551
katherine <116115224+kaerulean@users.noreply.github.com>
2024-06-05 13:29:50 -0400

    💟

--- paint/src/views/HomeView.vue
+++ paint/src/views/HomeView.vue
@@ -2,11 +2,16 @@
     
     <div class="home">
         <h1 class="micro-5-charted-regular" id="header">minecraft paint</h1>
+        <div><!--v-for something the array below--></div>
     </div>
-    
 </template>
 
 <script setup lang="ts">
+const images: string[] = []; //fix this strings later :skull:
+//for loop -> push each thing to the array thing
+images.push('hi')
+
+console.log(images)
 </script>
 
 <style>
