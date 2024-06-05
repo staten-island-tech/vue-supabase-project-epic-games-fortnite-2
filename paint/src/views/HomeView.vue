91ee78905a81e4e2b1bbccd2f32c7eb2d6c0625c
katherine <116115224+kaerulean@users.noreply.github.com>
2024-06-05 12:41:41 -0400

    RYAN TITLED THE THING

--- paint/src/views/HomeView.vue
+++ paint/src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <template>
+    
     <div class="home">
-        <h1>HOme</h1>
+        <h1 class="micro-5-charted-regular" id="header">minecraft paint</h1>
     </div>
+    
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped>
-div.home {
-    margin-top: 80px
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Micro+5+Charted&display=swap');
+.home {
+    margin-top: 6.1rem;
+    margin-left: 23rem;
+    text-align: center;
+}
+
+.micro-5-charted-regular {
+  font-family: "Micro 5 Charted", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+#header{
+    font-size: 10rem;
+    text-align: center;
 }
 </style>
